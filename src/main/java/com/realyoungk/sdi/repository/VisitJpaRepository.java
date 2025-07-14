6f55ec366240aa681a830d23a2ecbaa09426c159
realyoungk <youngjin@fooddash.co.kr>
2025-07-14 11:09:44 +0900

    remove: 불필요한 repository-annotation

--- src/main/java/com/realyoungk/sdi/repository/VisitJpaRepository.java
+++ src/main/java/com/realyoungk/sdi/repository/VisitJpaRepository.java
@@ -5,7 +5,5 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-@Primary
 public interface VisitJpaRepository extends JpaRepository<VisitEntity, String>, VisitRepository {
 }
