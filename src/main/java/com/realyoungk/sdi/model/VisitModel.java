8ee86c504fa6e6d0f438eb5bd476a929186a583b
realyoungk <youngjin@fooddash.co.kr>
2025-07-14 11:02:12 +0900

    refactor: lombok-data-annotation

--- src/main/java/com/realyoungk/sdi/model/VisitModel.java
+++ src/main/java/com/realyoungk/sdi/model/VisitModel.java
@@ -1,12 +1,16 @@
 package com.realyoungk.sdi.model;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
+@Getter
+@NoArgsConstructor
 // 탐방
 public class VisitModel {
+    private String id;
     // 시작시각
     private Date startedAt;
     // 끝나는시각
@@ -19,4 +23,15 @@ public class VisitModel {
     private String organizer;
     // 비고
     private String remark;
+
+    @Builder
+    public VisitModel(String id, Date startedAt, Date finishedAt, String participantCount, String teamName, String organizer, String remark) {
+        this.id = id;
+        this.startedAt = startedAt;
+        this.finishedAt = finishedAt;
+        this.participantCount = participantCount;
+        this.teamName = teamName;
+        this.organizer = organizer;
+        this.remark = remark;
+    }
 }
